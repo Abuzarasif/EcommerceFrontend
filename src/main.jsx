b40ada7dbd51072e25dd4f9b00704ab584c6a588
Abuzarasif <2020cs699@student.uet.edu.pk>
2025-05-21 05:21:15 +0500

    integrated login and logout feature with auth0

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import { GoogleOAuthProvider } from '@react-oauth/google';
+
+// import { Auth0Provider } from '@auth0/auth0-react';
+
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <GoogleOAuthProvider clientId="1018617522323-kdtccp3vn2rd75q060gti7d2ubpunkka.apps.googleusercontent.com">
+      
+//     <App />
+//       </GoogleOAuthProvider>;
+
+//   </StrictMode>,
+// )
+
+
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+import { Auth0Provider } from '@auth0/auth0-react';
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+<Auth0Provider
+    domain="dev-omn6fzzd4jlxowyw.us.auth0.com"
+    clientId="6qypoJywKqUFvoMqpsSRiN4eYHt6m7pH"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >      
     <App />
-  </StrictMode>,
+</Auth0Provider>
 )
